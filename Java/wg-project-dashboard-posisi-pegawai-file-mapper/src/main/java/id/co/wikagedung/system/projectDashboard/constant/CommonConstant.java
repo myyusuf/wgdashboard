cbaedff34cada75af36b0730ac7820dd9b7a0cf8
iedfian <iedfian.taufiq.akbar@gmail.com>
2015-11-24 15:54:18 +0700

    Add : Posisi Pegawai Java Model

---
+++ Java/wg-project-dashboard-posisi-pegawai-file-mapper/src/main/java/id/co/wikagedung/system/projectDashboard/constant/CommonConstant.java
@@ -0,0 +1,7 @@
+package id.co.wikagedung.system.projectDashboard.constant;
+
+public class CommonConstant {
+
+	public static String WG_POSISI_PEGAWAI_SHEET_CONSTANT = "pegawai";
+
+}
---
+++ Java/wg-project-dashboard-posisi-pegawai-file-mapper/src/main/java/id/co/wikagedung/system/projectDashboard/model/PosisiPegawai.java
@@ -0,0 +1,217 @@
+package id.co.wikagedung.system.projectDashboard.model;
+
+public class PosisiPegawai {
+
+	private ManajerProyek manajerProyek;
+	private SiteEngineer siteEngineer;
+	private Teknik teknik;
+	private QaQc qaQc;
+	private Drafter drafter;
+	private Surveyor surveyor;
+	private Danlat danlat;
+	private KasiKeuanganDanSdm kasiKeuanganDanSdm;
+	private StaffAkuntansiDanKeuangan staffAkuntansiDanKeuangan;
+	private StaffGudang staffGudang;
+	private DriverFasilitas driverFasilitas;
+	private Umum umum;
+	private ManajerKonstruksi manajerKonstruksi;
+	private PelaksanaUtama pelaksanaUtama;
+	private Pelaksana pelaksana;
+	private PelaksanaUtamaMep pelaksanaUtamaMep;
+	private PelaksanaMep pelaksanaMep;
+	private She she;
+
+	public PosisiPegawai() {
+	}
+
+	public PosisiPegawai(ManajerProyek manajerProyek,
+			SiteEngineer siteEngineer, Teknik teknik, QaQc qaQc,
+			Drafter drafter, Surveyor surveyor, Danlat danlat,
+			KasiKeuanganDanSdm kasiKeuanganDanSdm,
+			StaffAkuntansiDanKeuangan staffAkuntansiDanKeuangan,
+			StaffGudang staffGudang, DriverFasilitas driverFasilitas,
+			Umum umum, ManajerKonstruksi manajerKonstruksi,
+			PelaksanaUtama pelaksanaUtama, Pelaksana pelaksana,
+			PelaksanaUtamaMep pelaksanaUtamaMep, PelaksanaMep pelaksanaMep,
+			She she) {
+		this.manajerProyek = manajerProyek;
+		this.siteEngineer = siteEngineer;
+		this.teknik = teknik;
+		this.qaQc = qaQc;
+		this.drafter = drafter;
+		this.surveyor = surveyor;
+		this.danlat = danlat;
+		this.kasiKeuanganDanSdm = kasiKeuanganDanSdm;
+		this.staffAkuntansiDanKeuangan = staffAkuntansiDanKeuangan;
+		this.staffGudang = staffGudang;
+		this.driverFasilitas = driverFasilitas;
+		this.umum = umum;
+		this.manajerKonstruksi = manajerKonstruksi;
+		this.pelaksanaUtama = pelaksanaUtama;
+		this.pelaksana = pelaksana;
+		this.pelaksanaUtamaMep = pelaksanaUtamaMep;
+		this.pelaksanaMep = pelaksanaMep;
+		this.she = she;
+	}
+
+	public ManajerProyek getManajerProyek() {
+		return manajerProyek;
+	}
+
+	public void setManajerProyek(ManajerProyek manajerProyek) {
+		this.manajerProyek = manajerProyek;
+	}
+
+	public SiteEngineer getSiteEngineer() {
+		return siteEngineer;
+	}
+
+	public void setSiteEngineer(SiteEngineer siteEngineer) {
+		this.siteEngineer = siteEngineer;
+	}
+
+	public Teknik getTeknik() {
+		return teknik;
+	}
+
+	public void setTeknik(Teknik teknik) {
+		this.teknik = teknik;
+	}
+
+	public QaQc getQaQc() {
+		return qaQc;
+	}
+
+	public void setQaQc(QaQc qaQc) {
+		this.qaQc = qaQc;
+	}
+
+	public Drafter getDrafter() {
+		return drafter;
+	}
+
+	public void setDrafter(Drafter drafter) {
+		this.drafter = drafter;
+	}
+
+	public Surveyor getSurveyor() {
+		return surveyor;
+	}
+
+	public void setSurveyor(Surveyor surveyor) {
+		this.surveyor = surveyor;
+	}
+
+	public Danlat getDanlat() {
+		return danlat;
+	}
+
+	public void setDanlat(Danlat danlat) {
+		this.danlat = danlat;
+	}
+
+	public KasiKeuanganDanSdm getKasiKeuanganDanSdm() {
+		return kasiKeuanganDanSdm;
+	}
+
+	public void setKasiKeuanganDanSdm(KasiKeuanganDanSdm kasiKeuanganDanSdm) {
+		this.kasiKeuanganDanSdm = kasiKeuanganDanSdm;
+	}
+
+	public StaffAkuntansiDanKeuangan getStaffAkuntansiDanKeuangan() {
+		return staffAkuntansiDanKeuangan;
+	}
+
+	public void setStaffAkuntansiDanKeuangan(
+			StaffAkuntansiDanKeuangan staffAkuntansiDanKeuangan) {
+		this.staffAkuntansiDanKeuangan = staffAkuntansiDanKeuangan;
+	}
+
+	public StaffGudang getStaffGudang() {
+		return staffGudang;
+	}
+
+	public void setStaffGudang(StaffGudang staffGudang) {
+		this.staffGudang = staffGudang;
+	}
+
+	public DriverFasilitas getDriverFasilitas() {
+		return driverFasilitas;
+	}
+
+	public void setDriverFasilitas(DriverFasilitas driverFasilitas) {
+		this.driverFasilitas = driverFasilitas;
+	}
+
+	public Umum getUmum() {
+		return umum;
+	}
+
+	public void setUmum(Umum umum) {
+		this.umum = umum;
+	}
+
+	public ManajerKonstruksi getManajerKonstruksi() {
+		return manajerKonstruksi;
+	}
+
+	public void setManajerKonstruksi(ManajerKonstruksi manajerKonstruksi) {
+		this.manajerKonstruksi = manajerKonstruksi;
+	}
+
+	public PelaksanaUtama getPelaksanaUtama() {
+		return pelaksanaUtama;
+	}
+
+	public void setPelaksanaUtama(PelaksanaUtama pelaksanaUtama) {
+		this.pelaksanaUtama = pelaksanaUtama;
+	}
+
+	public Pelaksana getPelaksana() {
+		return pelaksana;
+	}
+
+	public void setPelaksana(Pelaksana pelaksana) {
+		this.pelaksana = pelaksana;
+	}
+
+	public PelaksanaUtamaMep getPelaksanaUtamaMep() {
+		return pelaksanaUtamaMep;
+	}
+
+	public void setPelaksanaUtamaMep(PelaksanaUtamaMep pelaksanaUtamaMep) {
+		this.pelaksanaUtamaMep = pelaksanaUtamaMep;
+	}
+
+	public PelaksanaMep getPelaksanaMep() {
+		return pelaksanaMep;
+	}
+
+	public void setPelaksanaMep(PelaksanaMep pelaksanaMep) {
+		this.pelaksanaMep = pelaksanaMep;
+	}
+
+	public She getShe() {
+		return she;
+	}
+
+	public void setShe(She she) {
+		this.she = she;
+	}
+
+	@Override
+	public String toString() {
+		return "PosisiPegawai [manajerProyek=" + manajerProyek
+				+ ", siteEngineer=" + siteEngineer + ", teknik=" + teknik
+				+ ", qaQc=" + qaQc + ", drafter=" + drafter + ", surveyor="
+				+ surveyor + ", danlat=" + danlat + ", kasiKeuanganDanSdm="
+				+ kasiKeuanganDanSdm + ", staffAkuntansiDanKeuangan="
+				+ staffAkuntansiDanKeuangan + ", staffGudang=" + staffGudang
+				+ ", driverFasilitas=" + driverFasilitas + ", umum=" + umum
+				+ ", manajerKonstruksi=" + manajerKonstruksi
+				+ ", pelaksanaUtama=" + pelaksanaUtama + ", pelaksana="
+				+ pelaksana + ", pelaksanaUtamaMep=" + pelaksanaUtamaMep
+				+ ", pelaksanaMep=" + pelaksanaMep + ", she=" + she + "]";
+	}
+
+}
