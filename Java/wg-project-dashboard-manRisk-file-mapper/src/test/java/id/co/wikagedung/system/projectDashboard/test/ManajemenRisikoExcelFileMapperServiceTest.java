a3801082bccbf72db9aa9399a4cdd2a08dd5a089
iedfian <iedfian.taufiq.akbar@gmail.com>
2015-12-07 12:11:49 +0700

    Add : Unit Test For ManRisk

--- Java/wg-project-dashboard-manRisk-file-mapper/src/test/java/id/co/wikagedung/system/projectDashboard/test/ManajemenRisikoExcelFileMapperServiceTest.java
+++ Java/wg-project-dashboard-manRisk-file-mapper/src/test/java/id/co/wikagedung/system/projectDashboard/test/ManajemenRisikoExcelFileMapperServiceTest.java
@@ -5,6 +5,7 @@ import id.co.wikagedung.system.projectDashboard.service.ManajemenRisikoExcelFile
 
 import java.io.IOException;
 import java.util.List;
+import java.util.UUID;
 
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.junit.Test;
@@ -20,7 +21,7 @@ public class ManajemenRisikoExcelFileMapperServiceTest {
 	@Autowired
 	private ManajemenRisikoExcelFileMapperService manajemenRisikoExcelFileMapperService;
 
-	@Test
+	// @Test
 	public void informasiManajemenExcelFileMapping() throws IOException,
 			InvalidFormatException {
 
@@ -33,6 +34,137 @@ public class ManajemenRisikoExcelFileMapperServiceTest {
 					+ manajemenRisiko.toString());
 
 		}
+
+	}
+
+	// @Test
+	public void getDataResikoEkstrimYangMasihOpen()
+			throws InvalidFormatException, IOException {
+
+		List<ManajemenRisiko> result = manajemenRisikoExcelFileMapperService
+				.manajemenRisikoExcelFileMapping();
+
+		int nomor = 0;
+
+		for (ManajemenRisiko manajemenRisiko : result) {
+
+			if (manajemenRisiko.getStlTingkatRisiko().equals("Ekstrim")) {
+
+				System.out.println(nomor + "." + " Nama Risiko : "
+						+ manajemenRisiko.getRisiko() + " , Nilai Risiko : "
+						+ manajemenRisiko.getStlNilaiRisiko()
+						+ " , Status Risiko : "
+						+ manajemenRisiko.getHtlStatus());
+
+				nomor++;
+
+			}
+
+		}
+
+	}
+
+	// @Test
+	public void getDataJumlahRisikoEsktrimPerProyek()
+			throws InvalidFormatException, IOException {
+
+		List<ManajemenRisiko> result = manajemenRisikoExcelFileMapperService
+				.manajemenRisikoExcelFileMapping();
+
+		int jumlahRisikoProyekSatu = 0;
+		int jumlahRisikoProyekDua = 0;
+
+		for (ManajemenRisiko manajemenRisiko : result) {
+
+			if (manajemenRisiko.getHtlStatus().equals("Open")) {
+
+				manajemenRisiko.setId(UUID.randomUUID().toString());
+				manajemenRisiko.setNamaProyek("Proyek 1");
+				manajemenRisiko.setPeriode("Desember-2015");
+
+			} else {
+
+				manajemenRisiko.setId(UUID.randomUUID().toString());
+				manajemenRisiko.setNamaProyek("Proyek 2");
+				manajemenRisiko.setPeriode("Desember-2015");
+
+			}
+
+		}
+
+		for (ManajemenRisiko manajemenRisiko : result) {
+
+			if ((manajemenRisiko.getNamaProyek().equals("Proyek 1"))
+					&& (manajemenRisiko.getStlTingkatRisiko().equals("Ekstrim"))) {
+
+				jumlahRisikoProyekSatu++;
+
+			} else if ((manajemenRisiko.getNamaProyek().equals("Proyek 2"))
+					&& (manajemenRisiko.getStlTingkatRisiko().equals("Ekstrim"))) {
+
+				jumlahRisikoProyekDua++;
+
+			}
+
+		}
+
+		System.out.println("Jumlah Risiko Ekstrim Proyek 1 : "
+				+ jumlahRisikoProyekSatu);
+		System.out.println("Jumlah Risiko Ekstrim Proyek 2 : "
+				+ jumlahRisikoProyekDua);
+
+	}
+
+	@Test
+	public void getDataJumlahProyekDenganRisikoEkstrim()
+			throws InvalidFormatException, IOException {
+
+		List<ManajemenRisiko> result = manajemenRisikoExcelFileMapperService
+				.manajemenRisikoExcelFileMapping();
+
+		int jumlahRisikoNovember = 0;
+		int jumlahRisikoDesember = 0;
+
+		for (ManajemenRisiko manajemenRisiko : result) {
+
+			if (manajemenRisiko.getHtlStatus().equals("Open")) {
+
+				manajemenRisiko.setId(UUID.randomUUID().toString());
+				manajemenRisiko.setNamaProyek("Proyek 1");
+				manajemenRisiko.setPeriode("Desember-2015");
+
+			} else {
+
+				manajemenRisiko.setId(UUID.randomUUID().toString());
+				manajemenRisiko.setNamaProyek("Proyek 2");
+				manajemenRisiko.setPeriode("November-2015");
+
+			}
+
+		}
+
+		for (ManajemenRisiko manajemenRisiko : result) {
+
+			if ((manajemenRisiko.getPeriode().equals("November-2015"))
+					&& (manajemenRisiko.getStlTingkatRisiko().equals("Ekstrim"))) {
+
+				jumlahRisikoNovember++;
+
+			} else if ((manajemenRisiko.getPeriode().equals("Desember-2015"))
+					&& (manajemenRisiko.getStlTingkatRisiko().equals("Ekstrim"))) {
+
+				jumlahRisikoDesember++;
+
+			}
+
+		}
+
+		System.out
+				.println("Jumlah Proyek Dengan Risiko Ekstrim Bulan November : "
+						+ jumlahRisikoNovember);
+		System.out
+				.println("Jumlah Proyek Dengan Risiko Ekstrim Bulan Desember : "
+						+ jumlahRisikoDesember);
 
 	}
 
