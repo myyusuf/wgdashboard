6d65d25fb7b9741e1a0a5c895ee23df23ab1cd3c
iedfian <iedfian.taufiq.akbar@gmail.com>
2015-12-13 14:32:30 +0700

    change sysout method

--- Java/wg-project-dashboard-labaRugi-file-mapper/src/test/java/id/co/wikagedung/system/projectDashboard/test/LabaRugiExcelFileMapperServiceTest.java
+++ Java/wg-project-dashboard-labaRugi-file-mapper/src/test/java/id/co/wikagedung/system/projectDashboard/test/LabaRugiExcelFileMapperServiceTest.java
@@ -2,6 +2,7 @@ package id.co.wikagedung.system.projectDashboard.test;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.UUID;
 
 import id.co.wikagedung.system.projectDashboard.model.LabaRugi;
 import id.co.wikagedung.system.projectDashboard.service.LabaRugiExcelFileMapperService;
@@ -25,7 +26,18 @@ public class LabaRugiExcelFileMapperServiceTest {
 		List<LabaRugi> result = labaRugiExcelFileMapperService.labaRugiExcelFileMapping();
 		
 		for(LabaRugi labaRugi:result){
-			System.out.println("LabaRugi: " + labaRugi.toString());
+			//System.out.println("LabaRugi: " + labaRugi.toString());
+			System.out.println("Id : " + UUID.randomUUID().toString());
+			System.out.println("Nama Proyek : " + "Proyek 1");
+			System.out.println("Periode : " + "Jan 2016");
+			System.out.println("Uraian : " + labaRugi.getKodeUraian());
+			System.out.println("RKP : " + labaRugi.getRkp());
+			System.out.println("S/D Tahun Lalu: " +labaRugi.getSampaiTahunLalu());
+			System.out.println("Tahun ini - Bulan Lalu : " + labaRugi.getBulanLalu());
+			System.out.println("Tahun ini - Bulan Ini : " + labaRugi.getBulanIni());
+			System.out.println("Tahun ini - S/D Bulan ini : " + labaRugi.getSampaiBulanIni());
+			System.out.println("S/D Tahun ini : " + labaRugi.getSampaiTahunIni());
+			System.out.println("---------------------------------------");
 		}
 	}
 }
