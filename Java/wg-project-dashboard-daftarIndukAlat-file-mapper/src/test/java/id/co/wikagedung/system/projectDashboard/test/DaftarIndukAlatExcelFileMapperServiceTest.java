93da1d2bc50b43cda8db33805a5d6f295f08c8e4
iedfian <iedfian.taufiq.akbar@gmail.com>
2015-12-13 14:33:06 +0700

    change sysout method

--- Java/wg-project-dashboard-daftarIndukAlat-file-mapper/src/test/java/id/co/wikagedung/system/projectDashboard/test/DaftarIndukAlatExcelFileMapperServiceTest.java
+++ Java/wg-project-dashboard-daftarIndukAlat-file-mapper/src/test/java/id/co/wikagedung/system/projectDashboard/test/DaftarIndukAlatExcelFileMapperServiceTest.java
@@ -2,11 +2,13 @@ package id.co.wikagedung.system.projectDashboard.test;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.UUID;
 
 import id.co.wikagedung.system.projectDashboard.model.DaftarIndukAlat;
 import id.co.wikagedung.system.projectDashboard.service.DaftarIndukAlatExcelFileMapperService;
 
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.util.SystemOutLogger;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,7 +28,23 @@ public class DaftarIndukAlatExcelFileMapperServiceTest {
 		List<DaftarIndukAlat> result = daftarIndukAlatExcelFileMapperService.daftarIndukAlatExcelFileMapping();
 		
 		for(DaftarIndukAlat daftarIndukAlat : result){
-			System.out.println("Daftar Induk Alat: " + daftarIndukAlat.toString());
+			System.out.println("Id : " + UUID.randomUUID().toString());
+			System.out.println("Nama Proyek : " + "Proyek 1");
+			System.out.println("Periode : " + "Jan 2016");
+			System.out.println("Keterangan : " + daftarIndukAlat.getKeterangan());
+			System.out.println("No Inventaris : " + daftarIndukAlat.getNoInventaris());
+			System.out.println("Nama Alat : " + daftarIndukAlat.getNamaAlat());
+			System.out.println("Merk Alat : " + daftarIndukAlat.getMerkAlat());
+			System.out.println("Model No Seri : " + daftarIndukAlat.getModelNoSeri());
+			System.out.println("Spesifikasi : " + daftarIndukAlat.getSpesifikasi());
+			System.out.println("Tahun Beli : " + daftarIndukAlat.getTahunBeli());
+			System.out.println("Nama Agen : " + daftarIndukAlat.getNamaAgen());
+			System.out.println("Alamat Agen : " + daftarIndukAlat.getAlamatAgen());
+			System.out.println("No Instruksi Kerja : " + daftarIndukAlat.getNoInstruksiKerja());
+			System.out.println("Lokasi ALat : " + daftarIndukAlat.getLokasiAlat());
+			System.out.println("-----------------------");
+			
+			
 		}
 	}
 
