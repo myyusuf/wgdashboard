290a671d56f97035687ce1dd0d60f3ec758c793a
iedfian <iedfian.taufiq.akbar@gmail.com>
2015-12-13 20:40:53 +0700

    change sysout method

--- Java/wg-project-dashboard-vo-file-mapper/src/test/java/id/co/wikagedung/system/projectDashboard/test/VoExcelFileMapperServiceTest.java
+++ Java/wg-project-dashboard-vo-file-mapper/src/test/java/id/co/wikagedung/system/projectDashboard/test/VoExcelFileMapperServiceTest.java
@@ -2,6 +2,7 @@ package id.co.wikagedung.system.projectDashboard.test;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.UUID;
 
 import id.co.wikagedung.system.projectDashboard.model.VO;
 import id.co.wikagedung.system.projectDashboard.model.VOResume;
@@ -25,7 +26,32 @@ public class VoExcelFileMapperServiceTest {
 	public void voExcelFileMapping() throws InvalidFormatException, IOException{
 		
 		VOResume result = voExcelFileMapperService.voResumeExcelFileMapping();
-		System.out.println("VO Resume: " + result.toString());
+		List<VO> vos = result.getVo();
+		System.out.println("Id : " + UUID.randomUUID().toString());
+		System.out.println("Nama Proyek : " + "Proyek 1");
+		System.out.println("Periode : " + "Jan 2016");
+		System.out.println("OK AWAL : " + result.getOkAwal());
+		System.out.println("VO YANG TELAH DISETUJUI / SERTIFIKAT : " + result.getVoDisetujui());
+		System.out.println("OK S/D SAAT INI : " + result.getOkSaatIni());
+		
+		System.out.println("---- Isi Tabel ----");
+		for(VO vo : vos){
+			System.out.println("Item Pekerjaan : " + vo.getItemPengerjaan());
+			System.out.println("No Surat Pengajuan : " + vo.getNoSuratPengajuan());
+			System.out.println("Tanggal Pengajuan : " + vo.getTanggalPengajuan());
+			System.out.println("Permohonan Yang Diajukan (Biaya) : " + vo.getPermohonanYangDiajukanBiaya());
+			System.out.println("Permohonan Yang Diajukan (Waktu) : " + vo.getPermohonanYangDiajukanWaktu());
+			System.out.println("Besarnya Claim Atas VO (Diajukan) : " + vo.getBesarnyaClaimAtasVODiajukan());
+			System.out.println("Besarnya Claim Atas VO (Negosiasi) : " + vo.getBesarnyaClaimAtasVONegosiasi());
+			System.out.println("Besarnya Claim Atas VO (Disetujui) : " + vo.getBesarnyaClaimAtasVODisetujui());
+			System.out.println("Besarnya Claim Atas VO (Diprogress) : " + vo.getBesarnyaClaimAtasVODiprogress());
+			System.out.println("Besarnya Claim Atas VO (Dibayar) : " + vo.getBesarnyaClaimAtasVODibayar());
+			System.out.println("Pelaksanaan (Sudah) : " + vo.getPelaksanaanSudah());
+			System.out.println("Pelaksanaan (Belum) : " + vo.getPelaksanaanBelum());
+			System.out.println("Keterangan : " + vo.getKeterangan());
+			System.out.println("-----------------------------");
+		}
+		//System.out.println("VO Resume: " + result.toString());
 	}
 
 }
